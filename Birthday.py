271ace94304eb3781aa2d3da4221f63ef84c76cc
Christian <Moore.Christian76@gmail.com>
2022-08-03 09:22:32 +0100

    added comments

--- Birthday.py
+++ Birthday.py
@@ -1,41 +1,43 @@
 from datetime import datetime
 
+# make birthday variable
 birthdate = {
     "day": 0,
     "month": 0,
     "year": 0
 }
 
+# make date variable
 Date = {
     "day": 0,
     "month": 0,
     "year": 0
 }
 
-def get_date():
+def get_date(): # sets date variable to datetime
     Date["day"] = datetime.now().day
     Date["month"] = datetime.now().month
     Date["year"] = datetime.now().year
 
-def get_birthday():
+def get_birthday(): # gets user birthday
 
     birthDay = int(input("Please state your birth day: ").strip())
     birthMonth = int(input("Please state your birth month (numerical): ").strip())
     birthYear = int(input("Please state your birth year (yyyy): ").strip())
     set_birthday(birthDay, birthMonth, birthYear)
 
-def set_birthday(birthDay, birthMonth, birthYear):
+def set_birthday(birthDay, birthMonth, birthYear): # sets birthday dict to acquired day, month, year from user
     birthdate["day"] = birthDay
     birthdate["month"] = birthMonth
     birthdate["year"] = birthYear
 
-def calculate_age():
+def calculate_age(): # compares birthday against date, then checks if birthday has passed or not, then prints tailored message
     age = Date["year"] - birthdate["year"]
     if Date["month"] <= birthdate["month"] and Date["day"] <= birthdate["day"]:
         age -= 1
     print("OMG", get_name() + "! you are", age, "years old. So you were born in", str(birthdate["year"]) + "!")
 
-def get_name():
+def get_name(): # assigns & returns the user's name from input
     name = input("Please enter your name: ").strip()
     return name
 
